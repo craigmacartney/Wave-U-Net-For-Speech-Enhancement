4f5a9b30ac9d0a32ef8e3d249636c14912de1e2d
MathewSam <m1sam@eng.ucsd.edu>
2019-03-03 17:02:22 -0800

    Data accumulation added

--- build_train_set.py
+++ build_train_set.py
@@ -1,15 +1,23 @@
 import os
 import argparse
+import wave
+import shutil 
 
 parser = argparse.ArgumentParser()
 parser.add_argument("clean_source")
 parser.add_argument("noisy_source")
+parser.add_argument("storage_folder")
 
 if __name__ =="__main__":
     args = parser.parse_args()
+    if not os.path.exists(args.storage_folder):
+        os.mkdir(args.storage_folder)
+    
     files = os.listdir(args.clean_source)
-    clean_files = set(["{0}/{1}".format(args.clean_source,file_name) for file_name in files])
-    files = os.listdir(args.noisy_source)
-    mixed_files = set(["{0}/{1}".format(args.noisy_source,file_name) for file_name in files])
-    print(clean_files - mixed_files)
-    print(mixed_files - clean_files)
+    for file_name in files:
+        if not os.path.exists("{0}/{1}".format(args.storage_folder,file_name[:4])):
+            os.mkdir("{0}/{1}".format(args.storage_folder,file_name[:4]))
+        shutil.copy2("{0}/{1}".format(args.clean_source,file_name),"{0}/{1}/clean.wav".format(args.storage_folder,file_name[:4]))
+        shutil.copy2("{0}/{1}".format(args.noisy_source,file_name),"{0}/{1}/mixed.wav".format(args.storage_folder,file_name[:4]))
+        
+    
